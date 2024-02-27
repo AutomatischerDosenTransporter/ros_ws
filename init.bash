7078685c70914a7fd9a44fddb9599b6e91a53f1e
DaNussi <daniel.nussbaum@htlstp.at>
2024-02-27 15:34:32 +0100

    ros_ws/init.bash

--- init.bash
+++ init.bash
@@ -1,4 +1,4 @@
 #!bash
 
 apt update -y
-apt install -y ros-humble-ros2-control ros-humble-ros2-controllers sudo apt install ros-humble-xacro ros-humble-foxglove-bridge
+apt install -y ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-xacro ros-humble-foxglove-bridge
