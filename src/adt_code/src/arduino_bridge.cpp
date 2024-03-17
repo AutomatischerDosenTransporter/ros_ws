256b8ebe6ef0f5933d85605977d0cdfffe7352e1
daniel.nussbaum@htlstp.at <daniel.nussbaum@htlstp.at>
2024-03-17 16:29:32 +0000

    src/adt_code/src/arduino_bridge.cpp

--- src/adt_code/src/arduino_bridge.cpp
+++ src/adt_code/src/arduino_bridge.cpp
@@ -9,24 +9,24 @@
 #include <libserial/SerialStream.h>
 #include "geometry_msgs/msg/twist.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "adt_code_msg/srv/arduino_command.hpp"
 
 using namespace std::chrono_literals;
-using std::placeholders::_1;
 using namespace LibSerial;
 
-class BasisRoboterMotorDriver : public rclcpp::Node
+class ArduinoBridge : public rclcpp::Node
 {
   public:
     LibSerial::SerialStream serial_port;
   
-    BasisRoboterMotorDriver()
-    : Node("basis_roboter_motor_driver")
+    ArduinoBridge()
+    : Node("arduino_bridge")
     {
         this->declare_parameter("serial_port", "/dev/ttyACM0");
-        this->declare_parameter("x_axis_scale", 1.0);
-        this->declare_parameter("y_axis_scale", 1.0);
-        this->declare_parameter("z_axis_scale", 1.0);
         std::string serial_port_device = this->get_parameter("serial_port").as_string();
+        
+        this->declare_parameter("command_topic", "command");
+        std::string command_topic = this->get_parameter("command_topic").as_string();
 
         serial_port.Open( serial_port_device );
 
@@ -35,12 +35,13 @@ class BasisRoboterMotorDriver : public rclcpp::Node
         serial_port.SetParity( Parity::PARITY_NONE );
         serial_port.SetStopBits( StopBits::STOP_BITS_1 ) ;
 
-        command_subscription_ = this->create_subscription<std_msgs::msg::String>("command", 10, std::bind(&BasisRoboterMotorDriver::command_callback, this, _1));
-        twist_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("command_twist", 10, std::bind(&BasisRoboterMotorDriver::twist_callback, this, _1));
+    
+        command_srv_ = this->create_service<adt_code_msg::srv::ArduinoCommand>(command_topic, std::bind(&ArduinoBridge::command_callback, this, std::placeholders::_1, std::placeholders::_2));
     }
 
   private:
-    bool send_command(const std::string & command) {
+    bool send_command(const std::string & command)
+    {
       std::stringstream ss;
       ss << command;
       serial_port << ss.str() << std::endl ;
@@ -78,51 +79,21 @@ class BasisRoboterMotorDriver : public rclcpp::Node
       }
     }
   
-    void command_callback(const std_msgs::msg::String & msg)
+    void command_callback(
+        const std::shared_ptr<adt_code_msg::srv::ArduinoCommand::Request> request,
+        std::shared_ptr<adt_code_msg::srv::ArduinoCommand::Response> response)
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-
-      std::string data(msg.data.c_str());
-      send_command(data);
+        RCLCPP_INFO(this->get_logger(), "Incoming request");
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr command_subscription_;
-
-    void twist_callback(const geometry_msgs::msg::Twist & msg)
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: TWIST");
-
-      double x = msg.linear.x;
-      double y = msg.linear.y;
-      double z = msg.linear.z;
-
-      if(x >  1.0) x =  1.0;
-      if(x < -1.0) x = -1.0;
-      if(y >  1.0) y =  1.0;
-      if(y < -1.0) y = -1.0;
-      if(z >  1.0) z =  1.0;
-      if(z < -1.0) z = -1.0;
-
-      x *= 100.0 * this->get_parameter("x_axis_scale").as_double();
-      y *= 100.0 * this->get_parameter("y_axis_scale").as_double();
-      z *= 100.0 * this->get_parameter("z_axis_scale").as_double();
-
-      std::string xData = "speed -axis x -speed " + std::to_string(x);
-      std::string yData = "speed -axis y -speed " + std::to_string(y);
-      std::string zData = "speed -axis z -speed " + std::to_string(z);
-      
-      send_command(xData);
-      send_command(yData);
-      send_command(zData);
-    }
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_subscription_;
-
+    rclcpp::Service<adt_code_msg::srv::ArduinoCommand>::SharedPtr command_srv_;
+  
 
 };
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<BasisRoboterMotorDriver>());
+  rclcpp::spin(std::make_shared<ArduinoBridge>());
   rclcpp::shutdown();
   return 0;
 }
